1e6b23779c1ff3c4db4455afca4f450a004b7954
Ahmad Kayyali <kayyali18@gmail.com>
2018-11-02 12:54:23 -0600

    Remove default example

--- app/i18n.js
+++ app/i18n.js
@@ -8,44 +8,39 @@
  *   You CANNOT use import/export in this file.
  */
 const addLocaleData = require('react-intl').addLocaleData; //eslint-disable-line
-const enLocaleData = require('react-intl/locale-data/en');
-const deLocaleData = require('react-intl/locale-data/de');
+const enLocaleData = require('react-intl/locale-data/en')
 
-const enTranslationMessages = require('./translations/en.json');
-const deTranslationMessages = require('./translations/de.json');
+const enTranslationMessages = require('./translations/en.json')
 
-addLocaleData(enLocaleData);
-addLocaleData(deLocaleData);
+addLocaleData(enLocaleData)
 
-const DEFAULT_LOCALE = 'en';
+const DEFAULT_LOCALE = 'en'
 
 // prettier-ignore
 const appLocales = [
   'en',
-  'de',
 ];
 
 const formatTranslationMessages = (locale, messages) => {
   const defaultFormattedMessages =
     locale !== DEFAULT_LOCALE
       ? formatTranslationMessages(DEFAULT_LOCALE, enTranslationMessages)
-      : {};
+      : {}
   const flattenFormattedMessages = (formattedMessages, key) => {
     const formattedMessage =
       !messages[key] && locale !== DEFAULT_LOCALE
         ? defaultFormattedMessages[key]
-        : messages[key];
-    return Object.assign(formattedMessages, { [key]: formattedMessage });
-  };
-  return Object.keys(messages).reduce(flattenFormattedMessages, {});
-};
+        : messages[key]
+    return Object.assign(formattedMessages, { [key]: formattedMessage })
+  }
+  return Object.keys(messages).reduce(flattenFormattedMessages, {})
+}
 
 const translationMessages = {
   en: formatTranslationMessages('en', enTranslationMessages),
-  de: formatTranslationMessages('de', deTranslationMessages),
-};
+}
 
-exports.appLocales = appLocales;
-exports.formatTranslationMessages = formatTranslationMessages;
-exports.translationMessages = translationMessages;
-exports.DEFAULT_LOCALE = DEFAULT_LOCALE;
+exports.appLocales = appLocales
+exports.formatTranslationMessages = formatTranslationMessages
+exports.translationMessages = translationMessages
+exports.DEFAULT_LOCALE = DEFAULT_LOCALE
